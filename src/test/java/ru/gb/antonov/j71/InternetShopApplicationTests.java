b1e4a8849c93e5522c93eef689a528c4f6c36b36
Topsy-Goo <topsygoo@yandex.ru>
2021-10-09 17:04:20 +0300

    Java7.Урок 7. Тестирование в Spring.ДЗ

--- src/test/java/ru/gb/antonov/j71/InternetShopApplicationTests.java
+++ src/test/java/ru/gb/antonov/j71/InternetShopApplicationTests.java
@@ -3,11 +3,8 @@ package ru.gb.antonov.j71;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class InternetShopApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+@SpringBootTest     //< связывает junit5 с контекстом Спринга
+class InternetShopApplicationTests
+{
+	@Test void contextLoads() {}
 }
